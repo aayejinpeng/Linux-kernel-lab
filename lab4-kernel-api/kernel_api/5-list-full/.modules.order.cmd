1a9a6d7368b5889b7fa6f1e5ce08a0d3939722bc
KnowNothing <46223982+aayejinpeng@users.noreply.github.com>
2021-08-03 17:39:23 +0800

    Delete .modules.order.cmd

--- lab4-kernel-api/kernel_api/5-list-full/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/5-list-full/modules.order := {   echo /home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/5-list-full/list-full.ko; :; } | awk '!x[$$0]++' - > /home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/5-list-full/modules.order
