2cf2830d342d4674bd0020afeca50100a039ea7b
KnowNothing <46223982+aayejinpeng@users.noreply.github.com>
2021-08-03 17:39:48 +0800

    Delete .list-sync.mod.cmd

--- lab4-kernel-api/kernel_api/6-list-sync/.list-sync.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/6-list-sync/list-sync.mod := { echo  /home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/6-list-sync/list-sync.o;  echo; } > /home/yjp/mylab/linux-kernel-lab/linux/tools/labs/skels/./kernel_api/6-list-sync/list-sync.mod
